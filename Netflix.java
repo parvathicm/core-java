161a584c328c039ac1efa3594f96aaa19b0ab347
parvathicm <you@example.com>
2025-06-30 14:19:07 +0530

    file

---
+++ Netflix.java
@@ -0,0 +1,14 @@
+class Netflix{
+	//int index =no of elements (length)-1
+	static String kannadaMovies [] = {"Milana","Mejestic","Galipata","Rajahuli","Yuvarathna","Paramathma",};
+		//one or more ref//
+	String teluguMovies []={"Sitaramam","Lucky Baskar","Subham",};
+	String hindiMovies []={"Sanam teri kasam","ashiki 2",};
+    String webSeries []={"Wednesday","Ranga SSLC","Puttnanja"};
+    String enlishMovie []={"Lost in love"};	
+	public static void main (String[] seriesCollection){
+		
+		System.out.println("The no of kannada movies added are "  + kannadaMovies.length);
+		System.out.println(kannadaMovies[0] + " "+ kannadaMovies[1]);
+	}	
+}
