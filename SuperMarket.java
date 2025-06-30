425bb4472b7544a38eed1d8e2abdcce9af557fd0
parvathicm <you@example.com>
2025-06-30 14:24:33 +0530

    file

---
+++ SuperMarket.java
@@ -0,0 +1,13 @@
+class SuperMarket{
+static String iceCreams[]={"Venilla","Chocolate","Strawberry","Rocky road","Soft serve","Butter pecan"};
+public static void main(String [] seriesCollection ){
+	//types of iceCrems
+System.out.println("The types of ice creams names " + iceCreams.length);
+System.out.println(iceCreams[0] + " " + iceCreams[1]+ " " + iceCreams[2] + " " + iceCreams[3] +" " + iceCreams[4] + " " + iceCreams[4]);
+}	
+static String purfumes[]={"Floral","Woody","Fresh","water girl","Fog"};
+public static void main(String [] seriesCollection ){
+System.out.println("The typs of perfumes names " + purfumes.length);
+System.out.println(perfumes[0] + " " + perfumes[1] + " " + perfumes[2] + " " + perfumes[3] + " " + perfumes[4]);
+}
+}
